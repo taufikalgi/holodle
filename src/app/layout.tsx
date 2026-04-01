b893bfb8072084b439a42c9900019ee742bf2639
Taufik Algi F <taufik.algi99@gmail.com>
2026-04-01 12:46:06 +0700

    fix: change metadata title and description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,9 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Hololive Wordle",
-  description: "Guess the Hololive talent! A daily guessing game.",
+  title: "Holodle - Hololive Wordle",
+  description:
+    "Think you know your Hololive talents? Prove it! Play once a day or go endless if you're obsessed.",
 };
 
 export default function RootLayout({
